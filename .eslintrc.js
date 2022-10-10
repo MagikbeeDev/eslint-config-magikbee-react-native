cf264aecffc267d47ca9fa397ee0bd182fdd70ed
diogoamiranda <diogo@magikbee.com>
2022-10-10 16:38:15 +0100

    initial version

---
+++ .eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  env: {
+    'react-native/react-native': true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json' // Required for Standard plugin
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-native"
+  ],
+  globals: {
+    __DEV__: false,
+    jasmine: false,
+    beforeAll: false,
+    afterAll: false,
+    beforeEach: false,
+    afterEach: false,
+    test: false,
+    expect: false,
+    describe: false,
+    jest: false,
+    it: false,
+  },
+  rules: {
+    'indent': ['error', 2],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+    ],
+    'padded-blocks': [
+      'error',
+      {
+        blocks: 'never',
+      },
+    ],
+    'brace-style': ['error', 'allman'],
+    'prefer-promise-reject-errors': 'off',
+    'camelcase': 0,
+    'comma-dangle': 0,
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'space-before-function-paren': 0,
+
+    "react/no-unescaped-entities": 0,
+    "react/prop-types": "off",
+    "react-native/no-raw-text": 0,
+
+    "@typescript-eslint/ban-ts-ignore": 0,
+    "@typescript-eslint/indent": 0,
+    "@typescript-eslint/explicit-member-accessibility": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/member-delimiter-style": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-object-literal-type-assertion": 0,
+    "@typescript-eslint/no-empty-interface": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/camelcase": 0
+  },
+}
+
---
+++ README.md
@@ -0,0 +1,64 @@
+# ESLint config for React Native projects
+
+- Use an [extension of eslint-config-standard, made for TypeScript](https://github.com/standard/eslint-config-standard-with-typescript)
+
+## Installing
+
+1. In your project folder, run:
+
+```
+npm install eslint @magikbee/eslint-config-magikbee-react-native --save-dev
+```
+
+2. Create (or update) a `.eslintrc` file in your project folder with the following content:
+
+```js
+{
+  'extends': [
+    '@magikbee/eslint-config-magikbee-react-native'
+  ]
+}
+```
+
+## How To Override Default Config
+
+1. Add your custom ESLint rules directly in .eslintrc file under "rules"
+
+## Extend TSConfig (Config required for Standard plugin)
+
+1. Create (or update) a `.tsconfig.json` file in your project folder and add the following content: 
+
+```json
+{
+  ...
+  "extends": "@magikbee/eslint-config-magikbee-react-native/.tsconfig.json", 
+  ...
+}
+```
+
+## Integration with VSCode
+
+1. Install [ESLint extension](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+
+2. Edit VSCode settings and edit or add the following settings:
+
+```json
+//tells the ESLint extension which languages it should check
+"eslint.validate": [
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact"
+],
+// avoid conflicts with eslint rules
+"editor.formatOnSave": false,
+// show eslint icon at bottom toolbar
+"eslint.alwaysShowStatus": true,
+// turns on Auto Fix for ESLint
+"editor.codeActionsOnSave": {
+  "source.fixAll.eslint": true
+}
+```
+
+- User settings: applied to all workspaces.
+- Workspace settings: only applied to the current workspace.
